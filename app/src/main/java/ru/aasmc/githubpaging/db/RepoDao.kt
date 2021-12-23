689d267ab10ec3daa840d89059da31c4bd367748
Alex Alexeyev <aasmc@yandex.ru>
2021-12-23 21:48:47 +0300

    Add Room database

---
+++ app/src/main/java/ru/aasmc/githubpaging/db/RepoDao.kt
@@ -0,0 +1,27 @@
+package ru.aasmc.githubpaging.db
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.aasmc.githubpaging.model.Repo
+
+@Dao
+interface RepoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(repos: List<Repo>)
+
+    @Query(
+        """
+        SELECT * FROM repos WHERE name LIKE :queryString 
+        OR description LIKE :queryString ORDER BY stars DESC, name ASC 
+    """
+    )
+    fun reposByName(queryString: String): PagingSource<Int, Repo>
+
+    @Query("DELETE FROM repos")
+    suspend fun clearRepos()
+
+}
